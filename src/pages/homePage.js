923bce1830109748967899e9b2af8190afb0dec0
lizhixiang <3045055246@qq.com>
2020-11-02 12:51:52 +0700

    Added filtering of movie list

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -4,6 +4,8 @@ import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
 const MovieListPage = () => {
+  const [titleFilter, setTitleFilter] = useState("");       
+  const [genreFilter, setGenreFilter] = useState("0");      
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
@@ -25,6 +27,28 @@ const MovieListPage = () => {
       <MovieList movies={movies} />
     </>
   );
-};
 
+// NEW BLOCK START
+const genre = Number(genreFilter)
+let displayedMovies = movies
+  .filter(m => {
+    return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+  })
+  .filter(m => {
+    return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+  });
+
+const handleFilterChange = (type, value) => {
+  if (type === "name") setTitleFilter(value);
+  else setGenreFilter(value);
+};
+// NEW BLOCK END
+return (
+  <>
+    <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
+    <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
+    <MovieList movies={displayedMovies} />                  {/* CHANGED */}
+  </>
+);
+};
 export default MovieListPage;
