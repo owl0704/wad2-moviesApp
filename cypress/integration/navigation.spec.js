72c1ec74de0512e62f3624bb8ad70d685bcbad76
lizhixiang <3045055246@qq.com>
2020-11-05 23:15:21 +0700

    E2E tests - Navigation - Favorites page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -104,4 +104,16 @@ describe("Navigation", () => {
       cy.url().should("include", `5f69e4d0cee2f6003633becf`);
     });
   });
+  describe("From the Favorites page", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get(".card").eq(0).find("button").click();
+      cy.get("nav").find("li").eq(2).find("a").click();
+    });
+    it("should navigate to the movies detail page and change the browser URL", () => {
+      cy.get(".card").eq(0).find("img").click();
+      cy.url().should("include", `/movies/${movies[1].id}`);
+      cy.get("h2").contains(movies[1].title);
+    });
+  });
 });
