85c7931478310ad2d85f9baedf45e79102a4db62
lizhixiang <3045055246@qq.com>
2020-11-05 21:19:22 +0700

    E2E tests - Navigation - Movies Details page

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,27 +1,49 @@
-import React, {useState, useEffect } from "react";
+import React from "react";
+import { Link, Route, withRouter } from "react-router-dom";
 import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
-import {getMovie} from '../api/tmdb-api'
+import MovieReviews from "../components/movieReviews";
+import useMovie from "../hooks/useMovie";
 
 const MoviePage = props => {
   const { id } = props.match.params;
-  const [movie, setMovie] = useState(null);
-  useEffect(() => {
-    getMovie(id).then(movie => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const [movie] = useMovie(id)  // NEW
   return (
     <>
-      {movie ? (
-          <PageTemplate movie={movie}>
-            <MovieDetails movie={movie} />
-          </PageTemplate>
-      ) : (
-        <p>Waiting for movie details</p>
-      )}
-    </>
+    {movie ? (
+      <>
+        <PageTemplate movie={movie}>
+          <MovieDetails movie={movie} />
+        </PageTemplate>
+        <div className="row">
+          <div className="col-12 ">
+            {!props.history.location.pathname.endsWith("/reviews") ? (
+              <Link
+                className="btn btn-primary btn-block active"
+                to={`/movies/${id}/reviews`}
+              >
+                Show Reviews (Extracts)
+              </Link>
+            ) : (
+              <Link
+                className="btn btn-primary btn-block active"
+                to={`/movies/${id}`}
+              >
+                Hide Reviews 
+              </Link>
+            )}
+          </div>
+        </div>
+        <Route
+          path={`/movies/:id/reviews`}
+          render={props => <MovieReviews movie={movie} {...props} />}
+        />
+      </>
+    ) : (
+      <p>Waiting for movie details</p>
+    )}
+  </>
   );
 };
 
-export default MoviePage;
+export default withRouter(MoviePage);
