180fb748837ad309887e30838fa38372b95fc8f8
lizhixiang <3045055246@qq.com>
2020-11-11 18:39:41 +0700

    Refactor: Movie list and genre list now stored in contexts

--- src/index.js
+++ src/index.js
@@ -7,26 +7,32 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import UpcomingMoviePage from "./pages/UpcomingMoviePage";
 import MoviePage from './pages/movieDetailsPage'
-import FavoriteMoviesPage from './pages/favoritesMoviesPage'  
+import FavoriteMoviesPage from './pages/favoritesMoviesPage' 
+import MoviesContextProvider from "./contexts/moviesContext";  
+import GenresContextProvider from "./contexts/genresContext";
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <div className="jumbotron">
-          <SiteHeader />      {/* New Header  */}
-          <div className="container-fluid">
-            <Switch>
+   <BrowserRouter>
+      <div className="jumbotron">
+        <SiteHeader /> 
+        <div className="container-fluid">
+          <MoviesContextProvider>     {/* NEW  */}
+          <GenresContextProvider>    {/* NEW */}
+            <Switch>    
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/upcoming" component={UpcomingMoviePage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
           <Redirect from="*" to="/" />
-        </Switch>
+          </Switch>
+          </GenresContextProvider>    {/* NEW */}
+          </MoviesContextProvider>     {/* NEW */}
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
-  );
+    </BrowserRouter>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
