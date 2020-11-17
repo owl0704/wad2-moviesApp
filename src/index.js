b0e472cb3cfc0e818398d8b713994cfdf5bc052e
lizhixiang <3045055246@qq.com>
2020-11-17 16:32:25 +0700

    CI - First build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import SiteHeader from './components/siteHeader'
 import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom" 
+import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom" 
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import UpcomingMoviePage from "./pages/UpcomingMoviePage";
